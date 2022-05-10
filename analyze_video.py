78a0f3cc37dfc5efcdcd90c9fe70233226a35343
FogRexOn <fogrexon@gmail.com>
2022-05-10 17:26:17 +0900

    create api

---
+++ analyze_video.py
@@ -0,0 +1,56 @@
+import cv2
+from DamageDetector import Detector as DamageDetector
+from EnemyDetector import Detector as EnemyDetector
+
+def analyze_video(video_controller, key, progress_callback):
+  video_info = video_controller.get_video_info()
+  damage_dropout = int(video_info["fps"] / 4)
+  enemy_dropout = int(video_info["fps"] / 2)
+  progress_dropout = int(video_info["fps"])
+  frame_infos = {
+    "fps": video_info["fps"],
+    "frame_count": video_info["frame_count"],
+    "frame_width": video_info["frame_width"],
+    "frame_height": video_info["frame_height"],
+    "damage_dropout": damage_dropout,
+    "enemy_dropout": enemy_dropout,
+    "damages": [],
+    "enemies": []
+  }
+  frame_count = video_info['frame_count']
+
+  damage_detector = DamageDetector(scale=0.5, key=key)
+  enemy_detector = EnemyDetector()
+  current_index = 0
+  video_controller.seek(current_index)
+  
+  for i in range(frame_count):
+    frame = video_controller.next()
+    if frame is None:
+      continue
+
+    # damage detection
+    if i % damage_dropout == 0:
+      damage_info = {"total": 0, "damages": []}
+      damages = damage_detector.get_damages(frame)
+      for damage in damages:
+        damage_info["total"] += damage["number"]
+        damage_info["damages"].append(damage)
+      frame_infos["damages"].append(damage_info)
+    
+    # enemy detection
+    if i % enemy_dropout == 0:
+      bboxes, clses, scores = enemy_detector.get_enemies(frame)
+      max_score_enemy = {"type": "none", "bbox": None, "score": 0}
+      for bbox, cls, score in zip(bboxes, clses, scores):
+        if score > 0.5 and score > max_score_enemy["score"]:
+          max_score_enemy = {"bbox": bbox, "type": cls, "score": score}
+      frame_infos["enemies"].append(max_score_enemy)
+
+    # progress callback
+    if i % progress_dropout == 0:
+      progress_callback(i / frame_count)
+
+  return frame_infos
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,55 @@
 import eel
+import wx
+import VideoController
+import analyze_video
+
+controller = None
+
+@eel.expose
+def get_local_file(wildcard="*"):
+  global controller
+  app = wx.App(None)
+  style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+  dialog = wx.FileDialog(None, 'Open', wildcard="*.mp4", style=style)
+  if dialog.ShowModal() == wx.ID_OK:
+      path = dialog.GetPath()
+  else:
+      path = None
+  dialog.Destroy()
+  controller = VideoController(path)
+  return path
+
+@eel.expose
+def get_frame():
+  global controller
+  return controller.next()
+@eel.expose
+def set_frame(index):
+  global controller
+  return controller.seek(index)
+
+def progress_callback(progress):
+  eel.progress(progress)
+
+@eel.expose
+def analyze(key):
+  global controller
+  if controller is None:
+    return None
+  analyze_video(controller, key, progress_callback)
+
+def handle_exit(a, b):
+  import sys
+  sys.exit(0)
 
 if __name__ == "__main__":
-  eel.init("web/app")
-  eel.start("index.html")
+  eel.init("gui/app")
+  eel.start(
+    "",
+    close_callback=handle_exit,
+    cmdline_args=[
+      '--disable-web-security',
+      '–-allow-file-access-from-files'
+    ],
+    port="8000"
+  )
